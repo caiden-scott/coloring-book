72d0ccbed6b01b4fb780cf88a8a64c440dfe9c6f
caiden-scott <caiden@amarillolibraryfriends.org>
2024-10-16 12:22:05 -0500

    Update page-1.js

--- page-1.js
+++ page-1.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById('coloringPage');
+const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const imageLoader = document.getElementById('imageLoader');
 const clearButton = document.getElementById('clearButton');
